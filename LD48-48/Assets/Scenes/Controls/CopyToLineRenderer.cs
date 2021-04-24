caf253d75b3300e93fa9c72e5dc164700aeeae78
Ariel Coppes <ariel.coppes@gmail.com>
2021-04-24 16:10:20 -0300

    added script to copy from bezier to linereanderer anda adde material for linerenderer

---
+++ LD48-48/Assets/Scenes/Controls/CopyToLineRenderer.cs
@@ -0,0 +1,30 @@
+using BezierSolution;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CopyToLineRenderer : MonoBehaviour
+{
+    public BezierSpline bezier;
+
+    public LineRenderer lineRenderer;
+
+    public int multiplyPoints = 1;
+
+    public Vector3 offset;
+
+    // Update is called once per frame
+    // [ContextMenu("Regenerate")]
+    private void LateUpdate()
+    {
+        var count = bezier.Count;
+        lineRenderer.positionCount = count * multiplyPoints;
+        // var positions = new Vector3[count];
+        for (var i = 0; i < lineRenderer.positionCount; i++)
+        {
+            var pointT = (i / (float) multiplyPoints) / (float) count;
+            lineRenderer.SetPosition(i, bezier.GetPoint(pointT) + offset);
+            // positions[i] = bezier[i].position;
+        }
+        // lineRenderer.SetPositions(positions);
+    }
+}
