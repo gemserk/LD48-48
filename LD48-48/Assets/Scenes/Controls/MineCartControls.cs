7450cceefeb63400172aa73ee4e924aacf3fd88e
Ariel Coppes <ariel.coppes@gmail.com>
2021-04-24 20:59:20 -0300

    proplayerism by using a trigger to attach to tracks

--- LD48-48/Assets/Scenes/Controls/MineCartControls.cs
+++ LD48-48/Assets/Scenes/Controls/MineCartControls.cs
@@ -11,11 +11,13 @@ namespace Scenes.Controls
 
         public Transform modelTransform;
         public Rigidbody rigidBody;
+        public Collider triggerCollider;
 
         public InputActionReference tiltActionRef;
         public InputActionReference jumpActionRef;
 
         public float tiltAngle = 15.0f;
+        public float jumpTiltXAngle = 25.0f;
 
         public float jumpTreshold = 0.1f;
         private bool attached => bezierWalker.spline != null;
@@ -30,13 +32,38 @@ namespace Scenes.Controls
         public float jumpForwardSpeedFactor = 0.5f;
         public float jumpUpVelocityFactor = 1.0f;
 
-        // public float timeToActivateRigidBody = 0.25f;
+        public float timeToActivateRigidBody = 0.25f;
         
         private float currentJumpDuration;
+        private float currentTimeToActivateRigidBody;
+
+        private bool waitingForColliderReattach;
         
         private void Start()
         {
             rigidBody.isKinematic = true;
+            // triggerCollider.enabled = false;
+        }
+
+        private void AttachToTrack(GameObject track)
+        {
+            var temporaryCopyRenderer = track.GetComponent<CopyToLineRenderer>();
+            if (temporaryCopyRenderer == null) 
+                return;
+            
+            bezierWalker.spline = temporaryCopyRenderer.bezier;
+                    
+            float normalizedT;
+            var bezierPosition =
+                temporaryCopyRenderer.bezier.FindNearestPointTo(transform.position, out normalizedT);
+        
+            transform.position = bezierPosition;
+            bezierWalker.NormalizedT = normalizedT;
+                    
+            bezierWalker.enabled = true;
+            rigidBody.isKinematic = true;
+
+            waitingForColliderReattach = false;
         }
 
         private void OnCollisionEnter(Collision other)
@@ -44,21 +71,23 @@ namespace Scenes.Controls
             var collider = other.collider;
             if (collider != null)
             {
-                var temporaryCopyRenderer = collider.gameObject.GetComponentInChildren<CopyToLineRenderer>();
-                if (temporaryCopyRenderer != null)
-                {
-                    bezierWalker.spline = temporaryCopyRenderer.bezier;
-                    
-                    float normalizedT;
-                    var bezierPosition =
-                        temporaryCopyRenderer.bezier.FindNearestPointTo(transform.position, out normalizedT);
+                AttachToTrack(collider.gameObject);
+            }
+        }
 
-                    transform.position = bezierPosition;
-                    bezierWalker.NormalizedT = normalizedT;
-                    
-                    bezierWalker.enabled = true;
-                    rigidBody.isKinematic = true;
-                }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!waitingForColliderReattach)
+                return;
+            AttachToTrack(other.gameObject);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            var temporaryCopyRenderer = other.gameObject.GetComponent<CopyToLineRenderer>();
+            if (temporaryCopyRenderer != null && temporaryCopyRenderer.bezier != null)
+            {
+                waitingForColliderReattach = true;
             }
         }
 
@@ -73,12 +102,13 @@ namespace Scenes.Controls
 
             if (!attached)
             {
-                localEulerAngles.x = -tiltAngle;
+                localEulerAngles.x = -jumpTiltXAngle;
             }
             
             modelTransform.localEulerAngles = localEulerAngles;
             
             currentJumpDuration -= Time.deltaTime;
+            currentTimeToActivateRigidBody -= Time.deltaTime;
 
             if (jumpValue > jumpTreshold)
             {
@@ -88,6 +118,8 @@ namespace Scenes.Controls
                     bezierWalker.spline = null;
                     
                     rigidBody.isKinematic = false;
+                    rigidBody.detectCollisions = false;
+                    
                     // attached = false;
                     
                     // set moving velocity (in x at lease
@@ -98,6 +130,7 @@ namespace Scenes.Controls
                     }
 
                     currentJumpDuration = jumpDuration;
+                    currentTimeToActivateRigidBody = timeToActivateRigidBody;
 
                     var forwardVelocity = transform.forward.normalized * bezierWalker.speed * jumpForwardSpeedFactor;
                     var verticalVelocity = Vector3.up * jumpUpVelocityFactor;
@@ -119,10 +152,21 @@ namespace Scenes.Controls
 
             if (!attached)
             {
+                if (currentTimeToActivateRigidBody < 0)
+                {
+                    rigidBody.detectCollisions = true;
+                    // triggerCollider.enabled = true;
+                }
+
+                var playerEuler = transform.localEulerAngles;
+                playerEuler.x = 0;
+                transform.localEulerAngles = playerEuler;
+                
                 // localEulerAngles = modelTransform.localEulerAngles;
                 // localEulerAngles.x = -tiltAngle;
                 // modelTransform.localEulerAngles = localEulerAngles;
                 rigidBody.AddForce(tiltVector.x * moveWhileJumpForce * Time.deltaTime, ForceMode.Force);
+
             }
 
             // if (jumpActionRef.action.triggered)
