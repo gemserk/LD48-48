3655fd3b7b12e406cc67cc1c9af877b18e11af36
Ruben Garat <ruben@ironhidegames.com>
2021-04-24 18:14:05 -0300

    added wall spawning, improved camera tracking in cavernscene

--- LD48-48/Assets/Game/Scripts/WallsSpawner.cs
+++ LD48-48/Assets/Game/Scripts/WallsSpawner.cs
@@ -19,16 +19,27 @@ public class WallsSpawner : MonoBehaviour
     public float floorDeltaMovement;
 
     public float floorSpawnChance;
-    
+
+    public float wallDistanceFromPathMin;
+    public float wallDistanceFromPathMax;
+    [Min(0.001f)]
+    public float wallDeltaMovement;
+    public float wallSpawnChance;
+    public List<GameObject> wallPrefabs;
+    public float wallYPos;
     
 
     public void Awake()
     {
         GenerateFloor();
+        GenerateWalls();
     }
 
     private void GenerateFloor()
     {
+        if (Mathf.Approximately(0, floorDeltaMovement))
+            return;
+        
         float normalizedT = 0;
         int iteration = 0;
         while (normalizedT < 1)
@@ -50,6 +61,31 @@ public class WallsSpawner : MonoBehaviour
             }
 
             iteration = (iteration + 1) % 2;
+        }
+    }
+    
+    private void GenerateWalls()
+    {
+        if (Mathf.Approximately(0, wallDeltaMovement))
+            return;
+        
+        float normalizedT = 0;
+        var wallMultipliers = new int[] {-1, 1};
+        while (normalizedT < 1)
+        {
+            Vector3 splinePos = spline.MoveAlongSpline(ref normalizedT, wallDeltaMovement);
+            
+            foreach (var wallMultiplier in wallMultipliers)
+            {
+                if (UnityEngine.Random.Range(0f, 1f) < wallSpawnChance)
+                {
+                    var distanceFromPath = UnityEngine.Random.Range(wallDistanceFromPathMin, wallDistanceFromPathMax);
+                    var xPos = splinePos.x + distanceFromPath * wallMultiplier;
+                    var zPos = splinePos.z;
+                    var yPos = splinePos.y + wallYPos;
+                    GameObject.Instantiate(wallPrefabs[0], new Vector3(xPos, yPos, zPos), Quaternion.identity);
+                }
+            }
         }
     }
 }
