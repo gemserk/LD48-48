b7bc2c6cf0b06547f6a191f97b2baaf2bbe5925e
Ariel Coppes <ariel.coppes@gmail.com>
2021-04-25 21:32:59 -0300

    floor hexas ping pong animation

--- LD48-48/Assets/Game/Scripts/MineObstacle.cs
+++ LD48-48/Assets/Game/Scripts/MineObstacle.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Game.Scripts
@@ -5,10 +6,54 @@ namespace Game.Scripts
     public class MineObstacle : MonoBehaviour
     {
         public Collider collider;
+
+        public bool animateModel;
+
+        public AnimationCurve randomSpeed;
         
+        public Vector3 animationDirection;
+
+        public AnimationCurve curve;
+        public Transform modelTransform;
+
+        private float animationSpeed;
+        private float time;
+        private float timeDirection = 1;
+
+        private void Awake()
+        {
+            if (animateModel)
+            {
+                time = UnityEngine.Random.Range(0.0f, 1.0f);
+                animationSpeed = randomSpeed.Evaluate(UnityEngine.Random.Range(0.0f, 1.0f));
+            }
+        }
+
         public void DisableCollisions()
         {
             collider.enabled = false;
+        }
+
+        private void LateUpdate()
+        {
+            if (!animateModel)
+            {
+                return;
+            }
+            
+            time += Time.deltaTime * timeDirection * animationSpeed;
+            var value = curve.Evaluate(time);
+            modelTransform.localPosition = animationDirection * value;
+
+            if (time > 1.0f)
+            {
+                timeDirection = -1f;
+            }
+
+            if (time < 0)
+            {
+                timeDirection = 1.0f;
+            }
         }
     }
 }
